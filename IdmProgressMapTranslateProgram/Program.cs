a8a67fdf925822cb75e05664939e67f8d7eb3eab
ryukinkou <ryukinkou@yahoo.co.jp>
2014-06-29 19:30:09 +0800

    test

---
+++ IdmProgressMapTranslateProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace IdmProgressMapTranslateProgram
+{
+    public class Program
+    {
+        //http://www.omg.org/spec/BPMN/20100501/Semantic.xsd
+        public static void Main(string[] args)
+        {
+
+            string sourcePath = Path.Combine(Environment.CurrentDirectory, @"source\progress map.vsdx");
+
+            Translator translator = new Translator();
+            translator.execute(sourcePath);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ IdmProgressMapTranslateProgram/Translator.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Office.Interop.Visio;
+
+namespace IdmProgressMapTranslateProgram
+{
+    class Translator
+    {
+
+        public void execute(string sourcePath)
+        {
+
+            Application application;
+
+            application = new Application();
+
+            Document doc;
+
+            doc = application.Documents.OpenEx(sourcePath, (short)Microsoft.Office.Interop.Visio.VisOpenSaveArgs.visOpenCopy);
+
+            Page page;
+
+            page = doc.Pages[1];
+
+            for (int i = 1; i <= page.Shapes.Count; i++)
+            {
+                Shape shape = page.Shapes[i];
+
+                //ROLE
+                if (shape.Name.Contains("Pool / Lane"))
+                {
+
+                }
+
+                if (shape.Name.Contains("Message Flow"))
+                {
+
+                    int sourceID = (int)shape.GluedShapes(VisGluedShapesFlags.visGluedShapesIncoming2D, "", null).GetValue(0);
+
+                    int destinationID = (int)shape.GluedShapes(VisGluedShapesFlags.visGluedShapesOutgoing2D, "", null).GetValue(0);
+
+                    Shape sourceShape = page.Shapes.get_ItemFromID(sourceID);
+
+                    Shape destinationShape = page.Shapes.get_ItemFromID(destinationID);
+
+                }
+
+                if (shape.Name.Contains("Task"))
+                {
+
+                }
+
+                if (shape.Name.Contains("Sheet"))
+                {
+
+                }
+
+                if (shape.Name.Contains("Data Object"))
+                {
+
+                }
+
+            }
+
+        }
+
+    }
+}
